4c769c998a59e569b2302a70de489f15b253b82f
redsled84 <hmnotsocool@gmail.com>
2016-03-29 12:10:53 -0700

    Collision resolution

--- main.lua
+++ main.lua
@@ -22,26 +22,28 @@ function Entity:move(dx, dy)
 	self.y = self.y + dy
 end
 
-function Entity:draw()
-	love.graphics.setColor(unpack(self.colors))
+function Entity:draw(color, type)
+	local type = type or 'line'
+	local color = color or self.color
+	love.graphics.setColor(unpack(color))
 	love.graphics.print(self.id, self.x+5,self.y+5)
-	love.graphics.rectangle("line", self.x, self.y, self.width, self.height)
+	love.graphics.rectangle(type, self.x, self.y, self.width, self.height)
 end
 
-function love.load()
-	math.randomseed(os.time())
-	math.random();math.random();math.random();
+local Rooms = {}
 
-	Rooms = {}
-	for k=1, 5 do
+function Rooms:loadRooms(n)
+	for k=1, n do
 		local tilesize = 32
 		local width, height = math.random(2, 6)*tilesize, math.random(2, 6)*tilesize
 		local x = math.abs(math.random(0, math.floor(love.graphics.getWidth()/tilesize) - width/tilesize)) * tilesize
 		local y = math.abs(math.random(0, math.floor(love.graphics.getHeight()/tilesize) - height/tilesize)) * tilesize
-		local room = Entity:new({x=x, y=y, width=width, height=height, colors={255,255,255}, id=k})	
+		local room = Entity:new({x=x, y=y, width=width, height=height, color={255,255,255}, id=k})	
 		table.insert(Rooms, room)
 	end
-	
+end
+
+function Rooms:collisionResolution()
 	local temp = shallowCopy(Rooms)
 	for k=1, #Rooms do
 		-- Remove the room that is being looped through
@@ -64,24 +66,30 @@ function love.load()
 			}
 			-- Importance: Ability to compare one room's values with the rest of the room's (needed for seperating colliding rooms)
 			if A.x1 < B.x2 and A.x2 > B.x1 and A.y1 < B.y2 and A.y2 > B.y1 then
-				print('collision checking is working!!')
+				local xDiff, yDiff = B.x2 - A.x1, B.y2 - A.y1
+				if xDiff < yDiff then
+					room.x = room.x + xDiff
+				else
+					room.y = room.y + yDiff
+				end
+				room.color = {0,0,255}
 			end
 		end	
 
 		-- Reset temp
 		temp = shallowCopy(Rooms)
 	end
-	--[[
-	Rooms = [A, B, C, D]
-	Temp = []
-	for i=1, #Rooms do
-		Temp = Rooms
-		table.remove(Temp, i)
-		local value = Rooms[i]
+end
 
+function love.load()
+	math.randomseed(os.time())
+	math.random();math.random();math.random();
 
-	end
-	]]
+	Rooms:loadRooms(10)
+end
+
+function love.update(dt)
+	Rooms:collisionResolution()
 end
 
 function love.draw()
