9bd640a75c5c2b2964f1996c5cc94b5f5d199265
redsled84 <hmnotsocool@gmail.com>
2016-04-07 20:25:16 -0700

    Code needs major optimization and fix kinks, but hey it kinda works in a way.

--- main.lua
+++ main.lua
@@ -1,7 +1,19 @@
 local inspect = require 'inspect'
+
+local meter = 64; love.physics.setMeter(meter)
+local world = love.physics.newWorld(0, 0 * meter, true)
+
+local tile_size = 32
+math.randomseed(os.time())
+math.random();math.random();math.random();
+
+local Physics = require 'physics'
 local gamera = require 'gamera'
-local cam = gamera.new(0,0,2000,2000)
-local tilesize = 32
+local cam = gamera.new(0-love.graphics.getWidth(),0-love.graphics.getHeight(),2000,2000)
+local Rooms = {}
+local positions = {}
+local blocks = {}
+local counter = 0
 
 local function shallowCopy(t)
 	local t2 = {}
@@ -9,98 +21,6 @@ local function shallowCopy(t)
 		t2[k] = t[k]
 	end
 	return t2
-end
--- things like player, door, enemies, etc...
-local Entity = {}
-local positions = {}
-
-function Entity:new(o)
-	o = o or {}
-	setmetatable(o, self)
-	self.__index = self
-	return o
-end
-
-function Entity:move(dx, dy)
-	self.x = self.x + dx
-	self.y = self.y + dy
-end
-
-function Entity:draw(color, type)
-	local type = type or 'line'
-	local color = color or self.color
-	love.graphics.setColor(unpack(color))
-	local str = tostring(self.id .. ' ' .. self.x .. ' ' .. self.y .. ' ' .. self.x+self.width .. ' ' .. self.y+self.height)
-	love.graphics.print(str, self.x+5,self.y+5)
-	love.graphics.rectangle(type, self.x, self.y, self.width, self.height)
-end
-
-local Rooms = {}
-
-function Rooms:loadRooms(n)
-	for k=1, n do
-		local tilesize = tilesize
-		local width, height = math.random(2, 6)*tilesize, math.random(2, 6)*tilesize
-		local x = math.abs(math.random(0, math.floor(love.graphics.getWidth()/tilesize) - width/tilesize)) * tilesize
-		local y = math.abs(math.random(0, math.floor(love.graphics.getHeight()/tilesize) - height/tilesize)) * tilesize
-		local room = Entity:new({
-			x = x,
-			y = y,
-			width = width,
-			height = height,
-			color = { 255, 255, 255, 200 },
-			doors = {},
-			id = k
-		})	
-		table.insert(Rooms, room)
-	end
-end
-
-function Rooms:loopRoomWithAllOtherRooms(f)
-	local temp = shallowCopy(Rooms)
-	for k=1, #Rooms do
-		-- Remove the room that is being looped through
-		table.remove(temp, k)
-		local room = Rooms[k]
-		local A = {
-			x1 = room.x,
-			y1 = room.y,
-			x2 = room.x+room.width,
-			y2 = room.y+room.height
-		}
-		-- Loop through all rooms except the one that is currently being looped above
-		for l=1, #temp do
-			local tempRoom = temp[l]
-			local B = {
-				x1 = tempRoom.x,
-				y1 = tempRoom.y,
-				x2 = tempRoom.x+tempRoom.width,
-				y2 = tempRoom.y+tempRoom.height
-			}
-			if room.id < tempRoom.id then
-				break
-			else
-				f(A, B, room, tempRoom)
-			end
-		end
-
-		temp = shallowCopy(Rooms)
-	end
-end
-
-function Rooms:collisionResolution()
-	self:loopRoomWithAllOtherRooms(function(A, B, room)
-		if A.x1 < B.x2 and A.x2 > B.x1 and A.y1 < B.y2 and A.y2 > B.y1 then
-			local xDiff, yDiff = B.x2 - A.x1, B.y2 - A.y1
-			if xDiff < yDiff then
-				room.x = room.x + xDiff
-			else
-				room.y = room.y + yDiff
-			end
-			room.color = {0,0,255, 200}
-			self:collisionResolution() -- yay recursion!!
-		end
-	end)
 end
 
 local function setPosition(a1, a2, b1, b2)
@@ -124,102 +44,211 @@ local function setPosition(a1, a2, b1, b2)
 	return a, b
 end
 
-function Rooms:checkSurroundingRooms()
-	self:loopRoomWithAllOtherRooms(function(A, B, room, tempRoom)
+function Rooms:checkAwesomeRooms(f)
+	local temp = shallowCopy(self)
+	for i=1, #self do
+		table.remove(temp, i)
+		local room = self[i]
+		local A = {
+			x1 = room.x,
+			y1 = room.y,
+			x2 = room.x+room.width,
+			y2 = room.y+room.height
+		}
+		for j=1, #temp do
+			local tempRoom = temp[j]
+			local B = {
+				x1 = tempRoom.x,
+				y1 = tempRoom.y,
+				x2 = tempRoom.x+tempRoom.width,
+				y2 = tempRoom.y+tempRoom.height
+			}
+			if room.id < tempRoom.id then
+				break
+			else
+				f(A, B, room, tempRoom)
+			end
+		end
+		temp = shallowCopy(Rooms)
+	end
+	counter = counter + 1
+end
+
+function Rooms:addPositions()
+	self:checkAwesomeRooms(function(A, B, room, tempRoom)
 		if A.x2 == B.x1 and A.y1 == B.y2 or -- A's top right corner touches B's bottom left corner
 			A.x1 == B.x2 and A.y1 == B.y2 or -- A's top left corner touches B's bottom right corner
 			A.x1 == B.x2 and A.y2 == B.y1 or -- A's bottom left corner touches B's top right corner
 			A.x2 == B.x1 and A.y2 == B.y1 then -- A's bottom right corner touches B's top left corner
 				-- empty because I don't want the corners to count as 'touching'
 		else
-			-- check if the rooms are even touching at all
-			if A.x1 - 1 < B.x2 and A.x2 + 1 > B.x1 and A.y1 - 1 < B.y2 and A.y2 + 1 > B.y1 then
-				if A.y1 + 1 < B.y2 and A.y2 - 1 > B.y1 then	-- Because the walls are only touching on the y-axis, when spawning doors we will use conditionals with the y positions
-					local x, y1, y2 = nil, nil, nil
-					-- In the case that only one of A's y-points is touching B, check which of A's point is touching and which of B's point is touching
-					-- In the case that both A's y-points are not touching, figure out if the room is bigger or smaller than B
-					-- If it's bigger, get both of B's y points
-					-- If it's smaller, get both of A's y points
-					-- A door will then be randomly spawned within these points
+			if A.x2 == B.x1 and A.y1 == B.y2 or -- A's top right corner touches B's bottom left corner
+				A.x1 == B.x2 and A.y1 == B.y2 or -- A's top left corner touches B's bottom right corner
+				A.x1 == B.x2 and A.y2 == B.y1 or -- A's bottom left corner touches B's top right corner
+				A.x2 == B.x1 and A.y2 == B.y1 then -- A's bottom right corner touches B's top left corner
+					-- empty because I don't want to do anything with the corners
+					room.isTouching = false
+			else
+				-- check if the rooms are even touching at all
+				if A.x1 - 1 < B.x2 and A.x2 + 1 > B.x1 and A.y1 - 1 < B.y2 and A.y2 + 1 > B.y1 then
+					if A.y1 + 1 < B.y2 and A.y2 - 1 > B.y1 then	
+						local x, y1, y2 = nil, nil, nil
+						y1, y2 = setPosition(A.y1+tile_size, A.y2-tile_size, B.y1+tile_size, B.y2-tile_size)
 
-					y1, y2 = setPosition(A.y1, A.y2, B.y1, B.y2)
+						print(y1, y2)
 
-					if A.x2 > B.x1 - 1 and A.x2 < B.x2 then -- A is touching B's LEFT wall
-						-- print(room.id .. ' is touching the LEFT side of ' .. tempRoom.id)
-						x = B.x1
-					elseif A.x1 < B.x2 + 1 and A.x1 > B.x1 then -- A is touching B's RIGHT wall
-						-- print(room.id .. ' is touching the RIGHT side of ' .. tempRoom.id)
-						x = B.x2
+						if A.x2 > B.x1 - 1 and A.x2 < B.x2 then -- A is touching B's LEFT wall
+							x = B.x1
+						elseif A.x1 < B.x2 + 1 and A.x1 > B.x1 then -- A is touching B's RIGHT wall
+							x = B.x2
+						end
+
+						if y1 > 0 and y2 > 0 and math.abs(y2 - y1) > tile_size then
+							local doorY = 0
+							if y2 - y1 - tile_size == 0 then
+								doorY = y1
+							else
+								doorY = (math.random(0, (y2-y1-tile_size)/tile_size) * tile_size) + y1
+							end
+							room.doors[#room.doors+1] = {x=x-tile_size, y=doorY, width=tile_size, height=tile_size, connectedTo=tempRoom.id, isSide=true}
+						end
+						x, y1, y2 = nil, nil, nil
 					end
+					if A.x1 + 1 < B.x2 and A.x2 - 1 > B.x1 then
+						local y, x1, x2 = nil, nil, nil
+						x1, x2 = setPosition(A.x1+tile_size, A.x2-tile_size, B.x1+tile_size, B.x2-tile_size)
+						if A.y2 > B.y1 - 1 and A.y2 < B.y2 then -- A is touching B's TOP wall
+							y = B.y1
+						elseif A.y1 < B.y2 + 1 and A.y1 > B.y1 then -- A is touching B's BOTTOM wall
+							y = B.y2
+						end
 
-					local doorY = 0
-					if y2 - y1 - tilesize == 0 then
-						doorY = y1
-					else
-						doorY = (math.random(0, (y2-y1-tilesize)/tilesize) * tilesize) + y1
+						if x1 > 0 and x2 > 0 and math.abs(x2 - x1) > tile_size then
+							local doorX = 0
+							if x2 - x1 - tile_size == 0 then
+								doorX = x1
+							else
+								doorX = (math.random(0, (x2-x1-tile_size)/tile_size) * tile_size) + x1
+							end
+							room.doors[#room.doors+1] = {x=doorX, y=y-tile_size, width=tile_size, height=tile_size, connectedTo=tempRoom.id, isSide=false}
+						end
+						y, x1, x2 = nil, nil, nil
 					end
-					room.doors[#room.doors+1] = Entity:new({x=x-tilesize/2, y=doorY, width=tilesize, height=tilesize})
-
-					positions[#positions+1] = {x = x, y1 = y1, y2 = y2}
-					x, y1, y2 = nil, nil, nil
-				end
-				if A.x1 + 1 < B.x2 and A.x2 - 1 > B.x1 then
-					local y, x1, x2 = nil, nil, nil
-					x1, x2 = setPosition(A.x1, A.x2, B.x1, B.x2)
-
-					if A.y2 > B.y1 - 1 and A.y2 < B.y2 then -- A is touching B's TOP wall
-						-- print(room.id .. ' is touching the TOP side of ' .. tempRoom.id)
-						y = B.y1
-					elseif A.y1 < B.y2 + 1 and A.y1 > B.y1 then -- A is touching B's BOTTOM wall
-						-- print(room.id .. ' is touching the BOTTOM side of ' .. tempRoom.id)
-						y = B.y2
-					end
-
-					local doorX = 0
-					if x2 - x1 - tilesize == 0 then
-						doorX = x1
-					else
-						doorX = (math.random(0, (x2-x1-tilesize)/tilesize) * tilesize) + x1
-					end
-					room.doors[#room.doors+1] = Entity:new({x=doorX, y=y-tilesize/2, width=tilesize, height=tilesize})
-
-					positions[#positions+1] = {y = y, x1 = x1, x2 = x2}
-					y, x1, x2 = nil, nil, nil
 				end
 			end
 		end
 	end)
 end
 
+function blocks:addBlocks()
+	for i=1, #Rooms do
+		local room = Rooms[i]
+			for j=0, room.width/tile_size-1 do
+				local x = room.x+j*tile_size
+				self[#self+1] = {x = x, y = room.y, width = tile_size, height = tile_size}
+				self[#self+1] = {x = x, y = room.y+(room.height/tile_size-1)*tile_size, width = tile_size, height = tile_size}
+			end
 
-local numOfRooms = 10
+			for j=1, room.height/tile_size-2 do
+				local y = room.y+j*tile_size
+				self[#self+1] = {x = room.x, y = y, width = tile_size, height = tile_size}
+				self[#self+1] = {x = room.x+(room.width/tile_size-1)*tile_size, y = y, width = tile_size, height = tile_size}
+			end
 
+	end
+end
+
+function blocks:getBlockIndex(x, y)
+	for i=1, #self do
+		local v = self[i]
+		if v.x == x and v.y == y then
+			return i
+		end
+	end
+end
+
+function blocks:removeBlockFromCoors(x, y)
+	local i = self:getBlockIndex(x, y)
+	self[i] = {}
+end
+
+function blocks:removeBlocks()
+	local counter = 0
+	for k=#self, 1, -1 do
+		local block = self[k]
+		for i=#Rooms, 1, -1 do
+			for j=#Rooms[i].doors, 1, -1 do
+				local door = Rooms[i].doors[j]
+				if door.x == block.x and door.y == block.y then
+					if not door.isSide then
+						blocks:removeBlockFromCoors(door.x, door.y+tile_size)
+					else
+						blocks:removeBlockFromCoors(door.x+tile_size, door.y)
+					end
+					self[k] = {}
+				end
+			end
+		end
+	end
+end
 
 function love.load()
-	math.randomseed(os.time())
-	math.random();math.random();math.random();
+	Physics:initialize()
 
-	Rooms:loadRooms(numOfRooms)
+	for i=1, 30 do
+		Physics:createRectangle(world, math.random(300,400),math.random(300,400),math.random(6,12)*tile_size,math.random(6,12)*tile_size)
+	end
 
-	Rooms:collisionResolution()
-	Rooms:checkSurroundingRooms()
+	for i=1, #Physics.bodies do
+		local v = Physics.bodies[i]
+		v.body:setFixedRotation(true)
+	end
+end
+
+local function AllAreAsleep()
+	local counter = 0
+	for i,v in ipairs(Physics.bodies) do
+		if v.body:isAwake() then
+			counter = counter + 1
+		end
+	end
+
+	if counter == #Physics.bodies then
+		return false
+	else
+		return true
+	end
 end
 
 function love.update(dt)
+	Physics:updateWorld(dt, world)
+
+	if counter == 0 and AllAreAsleep() then
+		for i=1, #Physics.bodies do
+			local v = Physics.bodies[i]
+			table.insert(Rooms, {x = v.x - v.x%tile_size, y = v.y - v.y % tile_size, width = v.width, height = v.height, id=i, doors = {}})
+		end
+		Physics:removeAllBodies()
+
+		Rooms:addPositions()
+		blocks:addBlocks()
+		blocks:removeBlocks()
+	end
+
 	local isDown = love.keyboard.isDown
 	local camX, camY = cam:getPosition()
 
-	if isDown("d") then
-		camX = camX + 100 * dt
+	if isDown("right") then
+		camX = camX + 200 * dt
 	end
-	if isDown("a") then
-		camX = camX - 100 * dt
+	if isDown("left") then
+		camX = camX - 200 * dt
 	end
-	if isDown("w") then
-		camY = camY - 100 * dt
+	if isDown("up") then
+		camY = camY - 200 * dt
 	end
-	if isDown("s") then
-		camY = camY + 100 * dt
+	if isDown("down") then
+		camY = camY + 200 * dt
 	end
 
 	cam:setPosition(camX, camY)
@@ -227,43 +256,39 @@ end
 
 function love.draw()
 	cam:draw(function(l,t,w,h)
-		for _, v in ipairs(Rooms) do
-			v:draw()
-			if #v.doors > 0 then
-				love.graphics.setColor(0,255,0, 100)
-				love.graphics.rectangle('fill', v.doors[1].x, v.doors[1].y, v.doors[1].width, v.doors[1].height)
-			end
-		end
+		Physics:drawPhysicsBodies(world)
 
-		-- for _, v in ipairs(positions) do
-		-- 	if v.x ~= nil then
-		-- 		love.graphics.setColor(255,0,0)
-		-- 		love.graphics.line(v.x, v.y1, v.x, v.y2)
-		-- 	elseif v.y ~= nil then
-		-- 		love.graphics.setColor(255,0,0)
-		-- 		love.graphics.line(v.x1, v.y, v.x2, v.y)
-		-- 	end
-		-- end
+		if #Rooms > 0 then
+			for _,v in ipairs(blocks) do
+				if v.x then
+				love.graphics.setColor(0,0,255,175)
+				love.graphics.rectangle('fill', v.x, v.y, v.width, v.height)
+				end
+			end
+			-- for _, v in ipairs(Rooms) do
+			-- 	love.graphics.setColor(255, 165, 0)
+			-- 	love.graphics.rectangle('line', v.x, v.y, v.width, v.height)
+			-- 	love.graphics.setColor(255,255,255)
+			-- 	love.graphics.print(tostring(v.id), v.x, v.y)
+			-- 	if #v.doors > 0 then
+			-- 		for j=1, #v.doors do
+			-- 			love.graphics.setColor(0,255,0, 100)
+			-- 			love.graphics.rectangle('line', v.doors[j].x, v.doors[j].y, v.doors[j].width, v.doors[j].height)
+			-- 			love.graphics.setColor(255,255,255)
+			-- 			love.graphics.print(tostring(v.doors[j].isSide), v.x+25, v.y)
+			-- 		end
+			-- 	end
+			-- end
+		end
 	end)
 end
 
 function love.keypressed(key)
-	if key == "r" then
-		for i=#Rooms, 1, -1 do
-			table.remove(Rooms, i)
-		end
-		for i=#positions, 1, -1 do
-			table.remove(positions, i)
-		end
-
-		Rooms:loadRooms(numOfRooms)
-
-		Rooms:collisionResolution()
-		Rooms:checkSurroundingRooms()
-
-		print(#positions)
+	if key == 'e' then
+		
 	end
-	if key == "escape" then
+
+	if key == 'escape' then
 		love.event.quit()
 	end
 end
--- main.old.lua
+++ main.old.lua
@@ -1,144 +1,269 @@
 local inspect = require 'inspect'
-local class = require 'middleclass'
+local gamera = require 'gamera'
+local cam = gamera.new(0,0,2000,2000)
+local tilesize = 32
 
-local Object = class('Object')
+local function shallowCopy(t)
+	local t2 = {}
+	for k=1, #t do
+		t2[k] = t[k]
+	end
+	return t2
+end
+-- things like player, door, enemies, etc...
+local Entity = {}
+local positions = {}
 
-function Object:initialize(o)
+function Entity:new(o)
 	o = o or {}
 	setmetatable(o, self)
 	self.__index = self
 	return o
 end
 
-
-local Room = class('Room', Object)
-
-math.randomseed(os.time())
-math.random();math.random();math.random()
-local tilesize = 32
-
-function Room:getRandomVars()
-	local tilesize = tilesize
-	local width = math.random(4, 12)
-	local height = math.random(4, 12)
-	local x = math.random(0, math.floor(love.graphics.getWidth()/tilesize) - width) * tilesize
-	local y = math.random(0, math.floor(love.graphics.getHeight()/tilesize) - height) * tilesize
-
-	return x, y, width, height, tilesize
+function Entity:move(dx, dy)
+	self.x = self.x + dx
+	self.y = self.y + dy
 end
 
-function Room:createRoom()
-	local x, y, width, height, tilesize = self:getRandomVars()
-	local room = self:initialize({
-		x = x,
-		y = y,
-		width = width,
-		height = height,
-		tilesize = tilesize,
-		walls = {},
-		features = {},
-		doors = {}
-	})
+function Entity:draw(color, type)
+	local type = type or 'line'
+	local color = color or self.color
+	love.graphics.setColor(unpack(color))
+	local str = tostring(self.id .. ' ' .. self.x .. ' ' .. self.y .. ' ' .. self.x+self.width .. ' ' .. self.y+self.height)
+	love.graphics.print(str, self.x+5,self.y+5)
+	love.graphics.rectangle(type, self.x, self.y, self.width, self.height)
+end
 
-	local ranNum = math.random(1,4)
-	if ranNum == 1 then
-		room.marked.N = true
-	elseif ranNum == 2 then
-		room.marked.E = true
-	elseif ranNum == 3 then
-		room.marked.S = true
-	elseif ranNum == 4 then
-		room.marked.W = true
+local Rooms = {}
+
+function Rooms:loadRooms(n)
+	for k=1, n do
+		local tilesize = tilesize
+		local width, height = math.random(2, 6)*tilesize, math.random(2, 6)*tilesize
+		local x = math.abs(math.random(0, math.floor(love.graphics.getWidth()/tilesize) - width/tilesize)) * tilesize
+		local y = math.abs(math.random(0, math.floor(love.graphics.getHeight()/tilesize) - height/tilesize)) * tilesize
+		local room = Entity:new({
+			x = x,
+			y = y,
+			width = width,
+			height = height,
+			color = { 255, 255, 255, 200 },
+			doors = {},
+			id = k
+		})	
+		table.insert(Rooms, room)
 	end
-
-	return room
 end
 
-local Door = class('Door', Object)
-
--- the way this function is designed will only allow one door to be added
-function Door:spawnDoor(room)
-	local ranOnX = math.random(2,room.width-1)
-	local ranOnY = math.random(2,room.height-1)
-
-	local door = self:initialize({
-		x = nil,
-		y = nil,
-		width = room.tilesize,
-		height = room.tilesize
-	})
-
-	if room.marked.N then
-		door.x = room.x + ranOnX * room.tilesize
-		door.y = room.y
-	elseif room.marked.S then
-		door.x = room.x + ranOnX * room.tilesize
-		door.y = room.y + (room.height-1) * room.tilesize
-	elseif room.marked.E then
-		door.x = room.x + (room.width-1) * room.tilesize
-		door.y = room.y + ranOnY * room.tilesize
-	elseif room.marked.W then
-		door.x = room.x
-		door.y = room.y + ranOnY * room.tilesize
-	end
-
-	return door
-end
-
-function Door:checkSpawn(roomt)
-	for i,v in ipairs(roomt) do
-		if v.marked.N then
-
-		elseif v.marked.S then
-
-		elseif v.marked.E then
-
-		elseif v.marked.W then
-
+function Rooms:loopRoomWithAllOtherRooms(f)
+	local temp = shallowCopy(Rooms)
+	for k=1, #Rooms do
+		-- Remove the room that is being looped through
+		table.remove(temp, k)
+		local room = Rooms[k]
+		local A = {
+			x1 = room.x,
+			y1 = room.y,
+			x2 = room.x+room.width,
+			y2 = room.y+room.height
+		}
+		-- Loop through all rooms except the one that is currently being looped above
+		for l=1, #temp do
+			local tempRoom = temp[l]
+			local B = {
+				x1 = tempRoom.x,
+				y1 = tempRoom.y,
+				x2 = tempRoom.x+tempRoom.width,
+				y2 = tempRoom.y+tempRoom.height
+			}
+			if room.id < tempRoom.id then
+				break
+			else
+				f(A, B, room, tempRoom)
+			end
 		end
+
+		temp = shallowCopy(Rooms)
 	end
 end
 
-local Corridor = class('Corridor', Object)
+function Rooms:collisionResolution()
+	self:loopRoomWithAllOtherRooms(function(A, B, room)
+		if A.x1 < B.x2 and A.x2 > B.x1 and A.y1 < B.y2 and A.y2 > B.y1 then
+			local xDiff, yDiff = B.x2 - A.x1, B.y2 - A.y1
+			if xDiff < yDiff then
+				room.x = room.x + xDiff
+			else
+				room.y = room.y + yDiff
+			end
+			room.color = {0,0,255, 200}
+			self:collisionResolution() -- yay recursion!!
+		end
+	end)
+end
+
+local function setPosition(a1, a2, b1, b2)
+	local a, b = 0, 0
+	if a1 <= b2 and a2 >= b2 then
+		a = a1
+		b = b2
+	end
+	if a1 <= b1 and a2 >= b1 then
+		a = a2
+		b = b1
+	end
+	if a2 <= b2 and a1 >= b1 then
+		a = a1
+		b = a2
+	end
+	if a1 <= b1 and a2 >= b2 then
+		a = b1
+		b = b2
+	end
+	return a, b
+end
+
+function Rooms:createDoors()
+	self:loopRoomWithAllOtherRooms(function(A, B, room, tempRoom)
+		if A.x2 == B.x1 and A.y1 == B.y2 or -- A's top right corner touches B's bottom left corner
+			A.x1 == B.x2 and A.y1 == B.y2 or -- A's top left corner touches B's bottom right corner
+			A.x1 == B.x2 and A.y2 == B.y1 or -- A's bottom left corner touches B's top right corner
+			A.x2 == B.x1 and A.y2 == B.y1 then -- A's bottom right corner touches B's top left corner
+				-- empty because I don't want the corners to count as 'touching'
+		else
+			-- check if the rooms are even touching at all
+			if A.x1 - 1 < B.x2 and A.x2 + 1 > B.x1 and A.y1 - 1 < B.y2 and A.y2 + 1 > B.y1 then
+				if A.y1 + 1 < B.y2 and A.y2 - 1 > B.y1 then	-- Because the walls are only touching on the y-axis, when spawning doors we will use conditionals with the y positions
+					local x, y1, y2 = nil, nil, nil
+					-- In the case that only one of A's y-points is touching B, check which of A's point is touching and which of B's point is touching
+					-- In the case that both A's y-points are not touching, figure out if the room is bigger or smaller than B
+					-- If it's bigger, get both of B's y points
+					-- If it's smaller, get both of A's y points
+					-- A door will then be randomly spawned within these points
+
+					y1, y2 = setPosition(A.y1, A.y2, B.y1, B.y2)
+
+					if A.x2 > B.x1 - 1 and A.x2 < B.x2 then -- A is touching B's LEFT wall
+						-- print(room.id .. ' is touching the LEFT side of ' .. tempRoom.id)
+						x = B.x1
+					elseif A.x1 < B.x2 + 1 and A.x1 > B.x1 then -- A is touching B's RIGHT wall
+						-- print(room.id .. ' is touching the RIGHT side of ' .. tempRoom.id)
+						x = B.x2
+					end
+
+					local doorY = 0
+					if y2 - y1 - tilesize == 0 then
+						doorY = y1
+					else
+						doorY = (math.random(0, (y2-y1-tilesize)/tilesize) * tilesize) + y1
+					end
+					room.doors[#room.doors+1] = Entity:new({x=x-tilesize/2, y=doorY, width=tilesize, height=tilesize})
+
+					positions[#positions+1] = {x = x, y1 = y1, y2 = y2}
+					x, y1, y2 = nil, nil, nil
+				end
+				if A.x1 + 1 < B.x2 and A.x2 - 1 > B.x1 then
+					local y, x1, x2 = nil, nil, nil
+					x1, x2 = setPosition(A.x1, A.x2, B.x1, B.x2)
+
+					if A.y2 > B.y1 - 1 and A.y2 < B.y2 then -- A is touching B's TOP wall
+						-- print(room.id .. ' is touching the TOP side of ' .. tempRoom.id)
+						y = B.y1
+					elseif A.y1 < B.y2 + 1 and A.y1 > B.y1 then -- A is touching B's BOTTOM wall
+						-- print(room.id .. ' is touching the BOTTOM side of ' .. tempRoom.id)
+						y = B.y2
+					end
+
+					local doorX = 0
+					if x2 - x1 - tilesize == 0 then
+						doorX = x1
+					else
+						doorX = (math.random(0, (x2-x1-tilesize)/tilesize) * tilesize) + x1
+					end
+					room.doors[#room.doors+1] = Entity:new({x=doorX, y=y-tilesize/2, width=tilesize, height=tilesize})
+
+					positions[#positions+1] = {y = y, x1 = x1, x2 = x2}
+					y, x1, x2 = nil, nil, nil
+				end
+			end
+		end
+	end)
+end
 
 
+local numOfRooms = 15
 
 
 function love.load()
-	base = Room:createRoom()
-	door = Door:spawnDoor(base)
-	base.doors[#base.doors+1] = door
-	print(inspect(base))
+	math.randomseed(os.time())
+	math.random();math.random();math.random();
+
+	Rooms:loadRooms(numOfRooms)
+
+	Rooms:collisionResolution()
+	Rooms:createDoors()
 end
 
 function love.update(dt)
+	local isDown = love.keyboard.isDown
+	local camX, camY = cam:getPosition()
 
+	if isDown("d") then
+		camX = camX + 100 * dt
+	end
+	if isDown("a") then
+		camX = camX - 100 * dt
+	end
+	if isDown("w") then
+		camY = camY - 100 * dt
+	end
+	if isDown("s") then
+		camY = camY + 100 * dt
+	end
+
+	cam:setPosition(camX, camY)
 end
 
 function love.draw()
-	love.graphics.setColor(255,255,255)
-	love.graphics.rectangle("fill", base.x, base.y, base.width*base.tilesize, base.height*base.tilesize)
-	love.graphics.setColor(0, 0, 255)
-	if base.marked.N then
-		love.graphics.rectangle('fill', base.x, base.y, base.width*base.tilesize, base.tilesize)
-	elseif base.marked.E then
-		love.graphics.rectangle('fill', base.x+(base.width-1)*base.tilesize, base.y, base.tilesize, base.height*base.tilesize)
-	elseif base.marked.S then
-		love.graphics.rectangle('fill', base.x, base.y+(base.height-1)*base.tilesize, base.width*base.tilesize, base.tilesize)
-	elseif base.marked.W then
-		love.graphics.rectangle('fill', base.x, base.y, base.tilesize, base.height*base.tilesize)
-	end
-	love.graphics.setColor(0,255,0)
-	love.graphics.rectangle('fill', door.x, door.y, door.width, door.height)
+	cam:draw(function(l,t,w,h)
+		for _, v in ipairs(Rooms) do
+			v:draw()
+			if #v.doors > 0 then
+				love.graphics.setColor(0,255,0, 100)
+				love.graphics.rectangle('fill', v.doors[1].x, v.doors[1].y, v.doors[1].width, v.doors[1].height)
+			end
+		end
+
+		-- for _, v in ipairs(positions) do
+		-- 	if v.x ~= nil then
+		-- 		love.graphics.setColor(255,0,0)
+		-- 		love.graphics.line(v.x, v.y1, v.x, v.y2)
+		-- 	elseif v.y ~= nil then
+		-- 		love.graphics.setColor(255,0,0)
+		-- 		love.graphics.line(v.x1, v.y, v.x2, v.y)
+		-- 	end
+		-- end
+	end)
 end
 
 function love.keypressed(key)
+	if key == "r" then
+		for i=#Rooms, 1, -1 do
+			table.remove(Rooms, i)
+		end
+		for i=#positions, 1, -1 do
+			table.remove(positions, i)
+		end
+
+		Rooms:loadRooms(numOfRooms)
+
+		Rooms:collisionResolution()
+		Rooms:createDoors()
+
+		print(#positions)
+	end
 	if key == "escape" then
 		love.event.quit()
-	end
-	if key == 'r' then
-		base = Room:createRoom()
-		door = Door:spawnDoor(base)
-		base.doors[#base.doors+1] = door
 	end
 end
